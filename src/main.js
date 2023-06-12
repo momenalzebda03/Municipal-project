84450ec174d929ff36eed4bb6b4e6e61b7c7f29f
unknown <momen07996@gmail.com>
2023-06-12 11:17:37 +0300

    create project and start

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "../dist/css/index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap";
+import "aos";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount("#app");
