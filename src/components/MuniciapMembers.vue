c7689b77b5f0aa538295679cd23fca13eb5f609f
unknown <momen07996@gmail.com>
2023-06-19 12:51:32 +0300

    update the members

--- src/components/MuniciapMembers.vue
+++ src/components/MuniciapMembers.vue
@@ -8,8 +8,9 @@
                     <div
                         class="div_left px-0 p-md-5 mt-4 d-flex justify-content-center align-items-center rounded-3 border border-2">
                         <div class="text-center">
-                            <img src="../assets/image_municipal2/PersonalBorder.png" alt="" class="mb-3">
-                            <p class="span_red fw-bold">م. هاشم سكيك</p>
+                            <img src="../assets/image_municipal2/PersonalBorder.png" alt=""
+                                class="mb-3 image_border_left rounded-circle">
+                            <p class="span_red fw-bold" id="text_left">م. هاشم سكيك</p>
                             <div class="d-flex justify-content-center">
                                 <div class="div_hr_center"></div>
                             </div>
@@ -17,7 +18,7 @@
                                 <p class="p_hr position-absolute px-3">عضو مجلس بلدي</p>
                             </div>
                             <p>ماجستير إدارة تشييد، مدير عام شركة بالكون للمقاولات والتجارة العامة، عضو مجلس إدارة اتحاد
-                                المقاولين، أمين سر مركز<br>التحكيم الهندسي، أمين سر مركز فض الخلافات</p>
+                                المقاولين، أمين سر مركز التحكيم الهندسي، أمين سر مركز فض الخلافات</p>
                         </div>
                     </div>
                 </div>
@@ -32,9 +33,9 @@
                     <div class="row text-center">
                         <div class="col-6 col-xl my-4">
                             <img src="../assets/image_municipal2/Personal1.png" alt=""
-                                class="mb-3 rounded-circle active_image image_pisel">
+                                class="mb-3 rounded-circle image_pisel">
                             <br>
-                            <span class="span_red fw-bold">م. هاشم سكيك</span>
+                            <span class="span_red text_poson text_poson fw-bold">م. هاشم سكيك</span>
                             <br>
                             <span>عضو مجلس بلدي</span>
                         </div>
@@ -42,7 +43,7 @@
                             <img src="../assets/image_municipal2/Personal2.png" alt=""
                                 class="mb-3 rounded-circle image_pisel">
                             <br>
-                            <span class="span_red fw-bold">أ. مروان الغول</span>
+                            <span class="span_red text_poson fw-bold">أ. مروان الغول</span>
                             <br>
                             <span>عضو مجلس بلدي</span>
                         </div>
@@ -50,7 +51,7 @@
                             <img src="../assets/image_municipal2/Personal3.png" alt=""
                                 class="mb-3 rounded-circle image_pisel">
                             <br>
-                            <span class="span_red fw-bold">د. جميل طرزي</span>
+                            <span class="span_red text_poson fw-bold">د. جميل طرزي</span>
                             <br>
                             <span>عضو مجلس بلدي</span>
                         </div>
@@ -58,7 +59,7 @@
                             <img src="../assets/image_municipal2/Personal4.png" alt=""
                                 class="mb-3 rounded-circle image_pisel">
                             <br>
-                            <span class="span_red fw-bold">أ. بدر صبرة</span>
+                            <span class="span_red text_poson fw-bold">أ. بدر صبرة</span>
                             <br>
                             <span>عضو مجلس بلدي</span>
                         </div>
@@ -66,7 +67,7 @@
                             <img src="../assets/image_municipal2/Personal5.png" alt=""
                                 class="mb-3 rounded-circle image_pisel">
                             <br>
-                            <span class="span_red fw-bold">أ. د. يحيى السراج</span>
+                            <span class="span_red text_poson fw-bold">أ. د. يحيى السراج</span>
                             <br>
                             <span>رئيس بلدية غزة</span>
                         </div>
@@ -74,7 +75,7 @@
                             <img src="../assets/image_municipal2/Personal6.png" alt=""
                                 class="mb-3 rounded-circle image_pisel">
                             <br>
-                            <span class="span_red fw-bold">أ. فداء المدهون</span>
+                            <span class="span_red text_poson fw-bold">أ. فداء المدهون</span>
                             <br>
                             <span>عضو مجلس بلدي</span>
                         </div>
@@ -82,7 +83,7 @@
                             <img src="../assets/image_municipal2/Personal7.png" alt=""
                                 class="mb-3 rounded-circle image_pisel">
                             <br>
-                            <span class="span_red fw-bold">أ. لؤي عياد</span>
+                            <span class="span_red text_poson fw-bold">أ. لؤي عياد</span>
                             <br>
                             <span>عضو مجلس بلدي</span>
                         </div>
@@ -90,7 +91,7 @@
                             <img src="../assets/image_municipal2/Personal8.png" alt=""
                                 class="mb-3 rounded-circle image_pisel">
                             <br>
-                            <span class="span_red fw-bold">أ. تامر الريس</span>
+                            <span class="span_red text_poson fw-bold">أ. تامر الريس</span>
                             <br>
                             <span>عضو مجلس بلدي</span>
                         </div>
@@ -98,7 +99,7 @@
                             <img src="../assets/image_municipal2/Personal9.png" alt=""
                                 class="mb-3 rounded-circle image_pisel">
                             <br>
-                            <span class="span_red fw-bold">م. إسماعيل حمادة</span>
+                            <span class="span_red text_poson fw-bold">م. إسماعيل حمادة</span>
                             <br>
                             <span>عضو مجلس بلدي</span>
                         </div>
@@ -106,7 +107,7 @@
                             <img src="../assets/image_municipal2/Personal10.png" alt=""
                                 class="mb-3 rounded-circle image_pisel">
                             <br>
-                            <span class="span_red fw-bold">أ. مصطفى قزعاط</span>
+                            <span class="span_red text_poson fw-bold">أ. مصطفى قزعاط</span>
                             <br>
                             <span>عضو مجلس بلدي</span>
                         </div>
@@ -127,6 +128,45 @@ export default {
     name: "KpapMembers",
     components: {
         KpapHeaderThreeTow
+    },
+    mounted() {
+        const image_pisel = document.querySelectorAll(".image_pisel");
+        const div_leftImage = document.querySelector(".div_left img");
+        const image_src = window.localStorage.getItem("image_src");
+        const image_border = window.localStorage.getItem("image_border");
+        const text_poson = document.querySelectorAll(".text_poson");
+        const text_left = document.getElementById("text_left");
+
+        if (image_src) {
+            div_leftImage.setAttribute("src", image_src);
+        }
+        const storedText = window.localStorage.getItem("text_left");
+        if (storedText) {
+            text_left.textContent = storedText;
+        }
+
+        image_pisel.forEach((image, index) => {
+            image.addEventListener("click", () => {
+                const title = text_poson[index].textContent;
+                text_left.textContent = title;
+                window.localStorage.setItem("text_left", title);
+                image_pisel.forEach((img) => {
+                    img.style.border = "none";
+                });
+                image.style.border = "5px solid #3EC4B5";
+                const newImageSrc = image.getAttribute("src");
+                div_leftImage.setAttribute("src", newImageSrc);
+                window.localStorage.setItem("image_src", newImageSrc);
+                window.localStorage.setItem("image_border", newImageSrc);
+            });
+        });
+
+        if (image_border && image_pisel.length) {
+            const borderedImage = Array.from(image_pisel).find((img) => img.getAttribute("src") === image_border);
+            if (borderedImage) {
+                borderedImage.style.border = "5px solid #3EC4B5";
+            }
+        }
     }
 }
 </script>
