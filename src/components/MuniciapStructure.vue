d715a9809a383c753c0d03792ddc1ce0e825f6c4
unknown <momen07996@gmail.com>
2023-06-23 03:22:17 +0300

    update the structure

--- src/components/MuniciapStructure.vue
+++ src/components/MuniciapStructure.vue
@@ -5,115 +5,158 @@
         <div class="d-flex justify-content-center" data-aos="flip-left">
             <h4 class="custom_class_red div_width_president py-3 div_mia fw-bold rounded-3">رئاسة البلدية</h4>
         </div>
-        <div class="row align-items-center mx-2 mx-sm-0">
-            <div class="col-12 col-sm-3 py-3 bg-white border border-2 rounded-3 shadow-sm" data-aos="fade-left">
-                <span class="fw-bold">دائرة الرقابة العامة</span>
+        <div class="row align-items-center mt-4">
+            <div class="col-12 col-md-6 col-lg-5 text-center text-md-end div_end_before" data-aos="fade-right">
+                <span class="fw-bold py-3 px-3 bg-white border border-2 rounded-3 shadow-sm">دائرة الرقابة العامة</span>
             </div>
-            <div class="col-12 col-sm-5 div_color_center"></div>
-            <div class="col-12 col-sm-3 mt-3 mt-sm-0 py-3 bg-white border border-2 rounded-3 shadow-sm"
-                data-aos="fade-right">
-                <span class="fw-bold">وحدة المستشار القانوني</span>
+            <div class="col-2 div_color_center div_center_none image_none"></div>
+            <div class="col-12 col-md-6 col-lg-5 text-center text-md-start mt-5 mt-md-0 div_start_before"
+                data-aos="fade-left">
+                <span class="fw-bold py-3 px-0 px-md-3 bg-white border border-2 rounded-3 shadow-sm">وحدة المستشار
+                    القانوني</span>
             </div>
         </div>
-        <div class="d-flex justify-content-center mt-3 mt-sm-0 mx-2 mx-sm-0">
+        <div class="d-flex justify-content-center mt-5 mx-2 mx-sm-0">
             <div class="px-3 px-sm-5 py-3 bg-white border border-2 rounded-3 d-inline shadow-sm position-absolute"
                 data-aos="fade-left">
                 <span class="fw-bold">دائرة خدمات الجمهور</span>
             </div>
         </div>
-        <div class="row mt-5 pt-5 mx-0">
-            <div class="col-12 div_color_center"></div>
-        </div>
-        <div class="row align-items-center mx-2 mx-sm-0 mt-5">
-            <div class="col text-start" data-aos="fade-right">
-                <div class="py-3 bg-white border border-2 rounded-3 shadow-sm d-inline px-5">
-                    <span class=" fw-bold">دائرة الرقابة العامة</span>
-                </div>
-            </div>
-            <div class="col mt-3 mt-sm-0" data-aos="fade-right">
-                <div class="py-3 bg-white border border-2 rounded-3 shadow-sm d-inline px-5">
-                    <span class=" fw-bold">وحدة المستشار القانوني</span>
-                </div>
-            </div>
-            <div class="col mt-3 mt-sm-0 text-end" data-aos="fade-right">
-                <div class="py-3 bg-white border border-2 rounded-3 shadow-sm d-inline px-5">
-                    <span class=" fw-bold">وحدة المستشار القانوني</span>
-                </div>
+        <div class="row mt-5 pt-5 mx-0 justify-content-center">
+            <div class="col-7 div_color_center image_none justify-content-between px-0">
+                <div class="div_before_after image_none"></div>
+                <div class="div_before_after image_none"></div>
             </div>
         </div>
-        <!-- <div class="d-flex justify-content-between align-items-center flex-lg-row flex-column px-5 px-lg-0 div_line_before">
-            <div class="px-5 py-3 bg-white border border-2 rounded-3 shadow-sm border_before1 mt-5" data-aos="fade-down">
-                <span class="fw-bold span_betfore">الإدارة العامة للخدمات</span>
-            </div>
-            <div class="px-5 py-3 bg-white border border-2 rounded-3 shadow-sm border_before2 mt-5" data-aos="fade-up">
-                <span class="fw-bold">الإدارة العامة للشؤون الإدارية</span>
-            </div>
-            <div class="px-5 py-3 bg-white border border-2 rounded-3 shadow-sm border_before1 mt-5" data-aos="fade-down">
-                <span class="fw-bold span_betfore">الإدارة العامة للشؤون الإدارية</span>
-            </div>
-        </div> -->
-        <!-- <div class="d-flex justify-content-between gap-lg-5 gap-0 flex-lg-row flex-column px-5 px-lg-0">
-            <div class="d-flex justify-content-end flex-lg-row flex-column gap-3 ps-0 pe-5 pe-sm-0 div_margin_padding1">
-                <div>
-                    <div class="px-5 py-3 mb-3 bg-white border border-2 rounded-3 shadow-sm border_before"
-                        data-aos="fade-left">
-                        <span class="fw-bold">الإدارة العامة<br>للمراكز الثقافية </span>
-                    </div>
-                    <div class="icon_before">
-                        <i class="fas fa-plus p-2 image_none rounded-circle div_active icon_background icon_click"></i>
-                    </div>
+        <div class="row align-items-center mt-3 mt-sm-4 justify-content-center">
+            <div class="col-12 col-md-3 p_bottom" data-aos="fade-up">
+                <div class="py-3 bg-white border border-2 rounded-3 shadow-sm d-inline px-2 px-lg-4">
+                    <span class="fw-bold">الإدارة العامة للخدمات</span>
                 </div>
-                <div>
-                    <div class="px-5 py-3 mb-3 bg-white border border-2 rounded-3 shadow-sm border_before"
-                        data-aos="fade-left">
-                        <span class="fw-bold">الإدارة العامة<br>للصحة والبيئة</span>
+                <div class="row mt-5 justify-content-center">
+                    <div class="col-7 div_color_center image_none justify-content-between px-0">
+                        <div class="div_before_after1 image_none">
+                            <div class="mt-5 pt-4 position-relative">
+                                <i
+                                    class="fas fa-plus icon_left p-2 position-absolute mt-5 image_none rounded-circle div_active icon_background icon_click"></i>
+                            </div>
+                        </div>
+                        <div class="div_before_after1 image_none">
+                            <div class="mt-5 pt-4 position-relative">
+                                <i
+                                    class="fas fa-plus icon_left p-2 position-absolute mt-5 image_none rounded-circle div_active icon_background icon_click"></i>
+                            </div>
+                        </div>
                     </div>
-                    <div class="icon_before">
-                        <i class="fas fa-plus p-2 image_none rounded-circle div_active icon_background icon_click"></i>
-                    </div>
-                </div>
-            </div>
-            <div class="d-flex justify-content-end flex-lg-row flex-column gap-3 ps-0 pe-5 pe-sm-0 div_margin_padding1">
-                <div>
-                    <div class="px-5 py-3 mb-3 bg-white border border-2 rounded-3 shadow-sm border_before"
-                        data-aos="fade-left">
-                        <span class="fw-bold">الإدارة العامة<br>للمراكز الثقافية </span>
-                    </div>
-                    <div class="icon_before">
-                        <i class="fas fa-plus p-2 image_none rounded-circle div_active icon_background icon_click"></i>
-                    </div>
-                </div>
-                <div>
-                    <div class="px-5 py-3 mb-3 bg-white border border-2 rounded-3 shadow-sm border_before"
-                        data-aos="fade-left">
-                        <span class="fw-bold">الإدارة العامة<br>للصحة والبيئة</span>
-                    </div>
-                    <div class="icon_before">
-                        <i class="fas fa-plus p-2 image_none rounded-circle div_active icon_background icon_click"></i>
+                    <div class="row px-0 mt-3 justify-content-center">
+                        <div class="col-12 col-md-6" data-aos="fade-right">
+                            <div class="py-3 bg-white border border-2 rounded-3 shadow-sm text-center">
+                                <span class="fw-bold">دائرة العلاقات<br>العامة والإعلام</span>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 mt-3 mt-md-0" data-aos="fade-left">
+                            <div class="py-3 bg-white border border-2 rounded-3 shadow-sm text-center">
+                                <span class="fw-bold">الإدارة العامة للمياه<br>والصرف الصحي</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="d-flex justify-content-end flex-lg-row flex-column gap-3 ps-0 pe-5 pe-sm-0 div_margin_padding1">
-                <div>
-                    <div class="px-5 py-3 mb-3 bg-white border border-2 rounded-3 shadow-sm border_before"
-                        data-aos="fade-left">
-                        <span class="fw-bold">الإدارة العامة<br>للمراكز الثقافية </span>
-                    </div>
-                    <div class="icon_before">
-                        <i class="fas fa-plus p-2 image_none rounded-circle div_active icon_background icon_click"></i>
-                    </div>
+            <div class="col-12 col-md-4 mt-3 py-5 py-md-0 mt-sm-0 p_bottom" data-aos="fade-donw">
+                <div class="py-3 bg-white border border-2 rounded-3 shadow-sm d-inline px-0 px-lg-5">
+                    <span class="fw-bold">الإدارة العامة للشؤون المالية</span>
                 </div>
-                <div>
-                    <div class="px-5 py-3 mb-3 bg-white border border-2 rounded-3 shadow-sm border_before"
-                        data-aos="fade-left">
-                        <span class="fw-bold">الإدارة العامة<br>للصحة والبيئة</span>
+                <div class="row mt-5 justify-content-center">
+                    <div class="col-7 div_color_center image_none justify-content-between px-0">
+                        <div class="div_before_after1 image_none">
+                            <div class="mt-5 pt-4 position-relative">
+                                <i
+                                    class="fas fa-plus icon_left p-2 position-absolute mt-5 image_none rounded-circle div_active icon_background icon_click"></i>
+                            </div>
+                        </div>
+                        <div class="div_before_after1 image_none">
+                            <div class="mt-5 pt-4 position-relative">
+                                <i
+                                    class="fas fa-plus icon_left p-2 position-absolute mt-5 image_none rounded-circle div_active icon_background icon_click"></i>
+                            </div>
+                        </div>
                     </div>
-                    <div class="icon_before">
-                        <i class="fas fa-plus p-2 image_none rounded-circle div_active icon_background icon_click"></i>
+                    <div class="row px-0 mt-3 justify-content-center">
+                        <div class="col-12 col-md-6" data-aos="fade-right">
+                            <div class="py-3 bg-white border border-2 rounded-3 shadow-sm text-center">
+                                <span class="fw-bold">الإدارة العامة<br>للتنظيم</span>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 mt-3 mt-md-0" data-aos="fade-left">
+                            <div class="py-3 bg-white border border-2 rounded-3 shadow-sm text-center">
+                                <span class="fw-bold">الإدارة العامة<br>للهندسة والتخطيط</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div> -->
+            <div class="col-12 col-md-3 mt-3" data-aos="fade-up">
+                <div class="py-3 bg-white border border-2 rounded-3 shadow-sm d-block d-lg-inline px-2">
+                    <span class="fw-bold">الإدارة العامة للشؤون الإدارية</span>
+                </div>
+                <div class="row mt-5 justify-content-center">
+                    <div class="col-7 div_color_center image_none justify-content-between px-0">
+                        <div class="div_before_after2 image_none">
+                            <div class="mt-5 pt-4 position-relative">
+                                <i
+                                    class="fas fa-plus icon_left p-2 position-absolute mt-5 image_none rounded-circle div_active icon_background icon_click"></i>
+                            </div>
+                        </div>
+                        <div class="div_before_after1 image_none">
+                            <div class="mt-5 pt-4 position-relative">
+                                <i
+                                    class="fas fa-plus icon_left p-2 position-absolute mt-5 image_none rounded-circle div_active icon_background icon_click"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row px-0 mt-3 justify-content-center">
+                        <div class="col-12 col-md-6" data-aos="fade-right">
+                            <div class="py-3 bg-white border border-2 rounded-3 shadow-sm text-center">
+                                <span class="fw-bold">الإدارة العامة<br>للمراكز الثقافية</span>
+                            </div>
+                            <div class="row mt-5 pt-4 justify-content-center">
+                                <div class="col-9 div_color_center image_none justify-content-between px-0">
+                                    <div class="div_before_after3 image_none">
+                                        <div class="mt-5 pt-5 position-relative">
+                                            <i
+                                                class="fas fa-plus icon_left p-2 position-absolute mt-5 image_none rounded-circle div_active icon_background icon_click"></i>
+                                        </div>
+                                    </div>
+                                    <div class="div_before_after3 image_none">
+                                        <div class="mt-5 pt-5 position-relative">
+                                            <i
+                                                class="fas fa-plus icon_left p-2 position-absolute mt-5 image_none rounded-circle div_active icon_background icon_click"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row px-0 mt-1 justify-content-center">
+                                    <div class="col-12 col-md-6" data-aos="fade-right">
+                                        <div class="py-3 bg-white border border-2 rounded-3 shadow-sm text-center">
+                                            <span class="fw-bold">الإدارة العامة<br>للمراكز الثقافية</span>
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-md-6 mt-3 mt-md-0" data-aos="fade-left">
+                                        <div class="py-3 bg-white border border-2 rounded-3 shadow-sm text-center">
+                                            <span class="fw-bold">الإدارة العامة<br>للمراكز الثقافية</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 mt-3 mt-md-0" data-aos="fade-left">
+                            <div class="py-3 bg-white border border-2 rounded-3 shadow-sm text-center">
+                                <span class="fw-bold">الإدارة العامة<br>للمراكز الثقافية</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
     <!-- end main -->
 </template>
